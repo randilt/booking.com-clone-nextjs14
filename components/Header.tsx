371b5b62989a3662c61c3225dc0d4ee64df175d7
RandilT <randiltharusha72@gmail.com>
2023-12-03 12:54:54 +0530

    Complete styling stays popup

--- components/Header.tsx
+++ components/Header.tsx
@@ -109,6 +109,21 @@ function Header() {
                     </div>
                   ))}
                 </div>
+                <div className="grid grid-cols-2 divide-x divide-gray-900 bg-gray-50">
+                  {callsToAction.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-[#013B94] hover:bg-gray-100"
+                    >
+                      <item.icon
+                        className="h-5 w-5 flex-none text-[#013B94]"
+                        aria-hidden="true"
+                      />
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
               </Popover.Panel>
             </Transition>
           </Popover>
